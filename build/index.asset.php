9c3f777afda46f73c5708bd4043d2e0b9adc1f9b
Jeremy Felt <jeremy.felt@gmail.com>
2024-02-23 12:23:40 -0800

    Update NPM dependencies, apply `npm run build`

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'e888e3c3dd8bf076ed45');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '753aeb01c55f76efa14d');
