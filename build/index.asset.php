7b3c649d07e743f00b5d62e8ac46509dde11e350
Jeremy Felt <jeremy.felt@gmail.com>
2024-02-23 13:11:09 -0800

    Bump block to apiVersion 3

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'b46f32b6e1aab7be0926');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'a9161f1911d6aa463149');
