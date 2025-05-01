e3f3479c3db67e3dbf97d9e175f990febdabb196
jeremyfelt <jeremy.felt@gmail.com>
2025-05-01 05:39:16 -0700

    Allow for a class on body when a toggle is toggled
    If a body class is specified for a toggle block, that class will
    be added to the document's body when that specific toggle block is
    toggled.
    
    Fixes #27

--- build/toggle-block/index.asset.php
+++ build/toggle-block/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '08086c18c6ed91df9b7f');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '258f20c46a12ec82d3d1');
