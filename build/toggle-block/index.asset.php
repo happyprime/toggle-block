92e7e5a0f09d4bed34221b9b4a3083c923ceb295
jeremyfelt <jeremy.felt@gmail.com>
2026-05-27 13:27:30 -0700

    Update block supports for WP 7.x

--- build/toggle-block/index.asset.php
+++ build/toggle-block/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n'), 'version' => '622cdfd76a7fc92b80d4');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n'), 'version' => '0e4927efa3a50df8255f');
